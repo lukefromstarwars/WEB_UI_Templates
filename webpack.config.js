7fcee391c40ad5041f254b8a473c9408c65666f3
lukefromstarwars <lukefromstarwars@gmail.com>
2018-03-30 10:00:27 +0200

    30032018 morning

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
 'use strict';
 
 // VARIABLES CONFIG
-const path = require('path');
-const glob = require('glob-all');
+const path   = require('path');
+const glob   = require('glob-all');
 const isProd = process.env.NODE_ENV === 'production';
 
-const webpack = require('webpack');
+const webpack           = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const PurifyCSSPlugin = require('purifycss-webpack');
-const GoogleFontsPlugin = require("google-fonts-webpack-plugin")
+const PurifyCSSPlugin   = require('purifycss-webpack');
+const GoogleFontsPlugin = require('google-fonts-webpack-plugin');
 
 const bootstrapEntryPoints = require('./webpack.bootstrap.config');
 
 const srcPaths = {
-	root: path.join(__dirname, 'src'),
-	img: path.join(__dirname, 'src/images'),
+	root     : path.join(__dirname, 'src'),
+	img      : path.join(__dirname, 'src/images'),
 	bootstrap: path.join(__dirname, 'src/bootstrap')
 };
 
 const distPaths = {
-	root: path.join(__dirname, 'dist'),
+	root : path.join(__dirname, 'dist'),
 	fonts: path.join(__dirname, 'dist/fonts'),
-	css: path.join(__dirname, 'dist/css')
-}
+	css  : path.join(__dirname, 'dist/css')
+};
 
 const fileNames = {
-	index: 'index.html',
+	index       : 'index.html',
 	registration: 'registration.html',
-	login: 'login.html'
+	login       : 'login.html'
 };
 
 // const filePaths = {
@@ -40,13 +40,12 @@ console.log(process.env.NODE_ENV);
 console.log(srcPaths.root);
 console.log(srcPaths.root);
 
-
 const cssDev = [
 	'style-loader',
 	'css-loader?sourceMap',
 	'postcss-loader',
 	{
-		loader: 'postcss-loader',
+		loader : 'postcss-loader',
 		options: {
 			plugins: function() {
 				return [require('autoprefixer')];
@@ -55,7 +54,7 @@ const cssDev = [
 	},
 	'sass-loader',
 	{
-		loader: 'sass-resources-loader',
+		loader : 'sass-resources-loader',
 		options: {
 			resources: ['./src/resources.scss']
 		}
@@ -64,12 +63,12 @@ const cssDev = [
 
 const cssProd = ExtractTextPlugin.extract({
 	fallback: 'style-loader',
-	use: [
+	use     : [
 		'css-loader',
 		'sass-loader',
 		'postcss-loader',
 		{
-			loader: 'postcss-loader',
+			loader : 'postcss-loader',
 			options: {
 				plugins: function() {
 					return [require('autoprefixer')];
@@ -77,7 +76,7 @@ const cssProd = ExtractTextPlugin.extract({
 			}
 		},
 		{
-			loader: 'sass-resources-loader',
+			loader : 'sass-resources-loader',
 			options: {
 				resources: ['./src/resources.scss']
 			}
@@ -88,55 +87,60 @@ const cssProd = ExtractTextPlugin.extract({
 
 // SERVER CONFIG
 const servDev = {
-	progress: true,
+	progress   : true,
 	contentBase: '.',
-	host: 'localhost',
-	port: 9000,
-	open: true,
-	stats: 'errors-only',
-	hot: true,
-	openPage: fileNames.login
+	host       : 'localhost',
+	port       : 9000,
+	open       : true,
+	stats      : 'errors-only',
+	hot        : true,
+	openPage   : fileNames.login
 };
 
 const servProd = {
-	progress: true,
-	compress: true,
+	progress   : true,
+	compress   : true,
 	contentBase: '.'
 };
 
 // SET CONFIG
-const cssConfig = isProd ? cssProd : cssDev;
-const serverConfig = isProd ? servProd : servDev;
-const devtoolConfig = isProd ? 'source-map' : 'eval';
-const bootstrapConfig = isProd ? bootstrapEntryPoints.prod : bootstrapEntryPoints.dev;
+const cssConfig       = isProd ? cssProd : cssDev;
+const serverConfig    = isProd ? servProd : servDev;
+const devtoolConfig   = isProd ? 'source-map' : 'eval';
+const bootstrapConfig = isProd
+	? bootstrapEntryPoints.prod
+	:   bootstrapEntryPoints.dev;
 
 module.exports = {
 	entry: {
 		app: srcPaths.root + '/app.js',
-		// fa: '@fortawesome/fontawesome-free-webfonts/scss/fontawesome.scss', 
+		// fa: '@fortawesome/fontawesome-free-webfonts/scss/fontawesome.scss',
 		// fa: srcPaths.src + '/fa.js',
-		jqueryValidation: ['jquery-validation', 'jquery-validation-unobtrusive'],
+		jqueryValidation: [
+			'jquery-validation',
+			'jquery-validation-unobtrusive'
+		],
 		bootstrap: bootstrapConfig
 	},
 	output: {
-		path: distPaths.root,
+		path    : distPaths.root,
 		filename: 'js/[name].bundle.js'
 	},
-	devtool: devtoolConfig,
+	devtool  : devtoolConfig,
 	devServer: serverConfig,
-	module: {
+	module   : {
 		rules: [
 			{
 				test: /\.scss$/,
-				use: cssConfig
+				use : cssConfig
 			},
 			{
 				test: /.(ttf|otf|eot|woff(2)?)(\?[a-z0-9]+)?$/,
-				use: [
+				use : [
 					{
-						loader: 'file-loader',
+						loader : 'file-loader',
 						options: {
-							name: '[name].[ext]',
+							name      : '[name].[ext]',
 							outputPath: 'fonts/',
 							publicPath: '../fonts/'
 						}
@@ -145,11 +149,11 @@ module.exports = {
 			},
 			{
 				test: /\.(jpe?g|png|gif|svg|ico)$/i,
-				use: [
+				use : [
 					{
-						loader: 'file-loader',
+						loader : 'file-loader',
 						options: {
-							name: '[name].[ext]',
+							name      : '[name].[ext]',
 							outputPath: 'assets/',
 							publicPath: '../assets/'
 						}
@@ -181,100 +185,103 @@ module.exports = {
 
 			// Bootstrap 4
 			{
-				test: /bootstrap\/dist\/js\/umd\//,
+				// prettier-ignore
+				test  : /bootstrap\/dist\/js\/umd\/$/,
 				loader: 'imports-loader?jQuery=jquery'
 			}
 		]
 	},
 	resolve: {
 		extensions: ['.js'],
-		modules: [path.resolve(__dirname, 'scripts'), 'node_modules'],
-		alias: {
-			'@fortawesome/fontawesome-free-solid$': '@fortawesome/fontawesome-free-solid/shakable.es.js',
-			'@fortawesome/fontawesome-free-regular$': '@fortawesome/fontawesome-free-regular/shakable.es.js'
+		modules   : [path.resolve(__dirname, 'scripts'), 'node_modules'],
+		alias     : {
+			'@fortawesome/fontawesome-free-solid$': 
+				'@fortawesome/fontawesome-free-solid/shakable.es.js',
+			'@fortawesome/fontawesome-free-regular$': 
+				'@fortawesome/fontawesome-free-regular/shakable.es.js'
 		}
 	},
 	plugins: [
 		new webpack.optimize.CommonsChunkPlugin({
-			names: ['app', 'jqueryValidation'],
+			names    : ['app', 'jqueryValidation'],
 			minChunks: 2
 		}),
 
 		new webpack.ProvidePlugin({
-			$: 'jquery',
-			jQuery: 'jquery',
+			$              : 'jquery',
+			jQuery         : 'jquery',
 			'window.jQuery': 'jquery',
-			Tether: 'tether',
+			Tether         : 'tether',
 			'window.Tether': 'tether',
-			Popper: ['popper.js', 'default'],
-			Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
-			Button: 'exports-loader?Button!bootstrap/js/dist/button',
-			Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
-			Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
-			Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
-			Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
-			Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
-			Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
-			Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
-			Tooltip: 'exports-loader?Tooltip!bootstrap/js/dist/tooltip',
-			Util: 'exports-loader?Util!bootstrap/js/dist/util'
+			Popper         : ['popper.js', 'default'],
+			Alert          : 'exports-loader?Alert!bootstrap/js/dist/alert',
+			Button         : 'exports-loader?Button!bootstrap/js/dist/button',
+			Carousel       : 'exports-loader?Carousel!bootstrap/js/dist/carousel',
+			Collapse       : 'exports-loader?Collapse!bootstrap/js/dist/collapse',
+			Dropdown       : 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
+			Modal          : 'exports-loader?Modal!bootstrap/js/dist/modal',
+			Popover        : 'exports-loader?Popover!bootstrap/js/dist/popover',
+			Scrollspy      : 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
+			Tab            : 'exports-loader?Tab!bootstrap/js/dist/tab',
+			Tooltip        : 'exports-loader?Tooltip!bootstrap/js/dist/tooltip',
+			Util           : 'exports-loader?Util!bootstrap/js/dist/util'
 		}),
 
 		new GoogleFontsPlugin({
 			fonts: [
-				{ family: "Source Sans Pro" },
-				{ family: "Roboto" },
-				{ family: "Roboto Condensed" },
-				{ family: "Ubuntu" },
+				{ family: 'Source Sans Pro' },
+				{ family: 'Roboto' },
+				{ family: 'Roboto Condensed' },
+				{ family: 'Ubuntu' }
 			],
-			path: 'fonts/',
-			minify: true,
+			path    : 'fonts/',
+			minify  : true,
 			filename: 'css/fonts.css'
 		}),
 
 		new HtmlWebpackPlugin({
-			title: 'Welcome',
+			title   : 'Welcome',
 			template: srcPaths.root + '/' + fileNames.index,
 			filename: fileNames.index,
-			favicon: srcPaths.root + '/images/favicon.ico',
-			minify: {
+			favicon : srcPaths.root + '/images/favicon.ico',
+			minify  : {
 				collapseWhitespace: false,
-				minifyJS: isProd,
-				minifyCSS: isProd
+				minifyJS          : isProd,
+				minifyCSS         : isProd
 			},
 			// hash      : isProd,
 			showErrors: !isProd
 		}),
 
 		new HtmlWebpackPlugin({
-			title: 'Login',
+			title   : 'Login',
 			template: srcPaths.root + '\\' + fileNames.login,
 			filename: fileNames.login,
-			minify: {
+			minify  : {
 				collapseWhitespace: false,
-				minifyJS: isProd,
-				minifyCSS: isProd
+				minifyJS          : isProd,
+				minifyCSS         : isProd
 			},
 			// hash      : isProd,
 			showErrors: !isProd
 		}),
 
 		new HtmlWebpackPlugin({
-			title: 'Registration',
+			title   : 'Registration',
 			template: srcPaths.root + '\\' + fileNames.registration,
 			filename: fileNames.registration,
-			minify: {
+			minify  : {
 				collapseWhitespace: false,
-				minifyJS: isProd,
-				minifyCSS: isProd
+				minifyJS          : isProd,
+				minifyCSS         : isProd
 			},
 			// hash      : isProd,
 			showErrors: !isProd
 		}),
 
 		new ExtractTextPlugin({
-			filename: 'css/[name].css',
-			disable: !isProd,
+			filename : 'css/[name].css',
+			disable  : !isProd,
 			allChunks: true
 		}),
 
@@ -285,10 +292,10 @@ module.exports = {
 		// Make sure this is after ExtractTextPlugin!
 		new PurifyCSSPlugin({
 			// Give paths to parse for rules. These should be absolute!
-			paths: glob.sync([path.join(__dirname, 'src/*.html')]),
+			paths        : glob.sync([path.join(__dirname, 'src/*.html')]),
 			purifyOptions: {
-				info: true,
-				minify: isProd,
+				info     : true,
+				minify   : isProd,
 				whitelist: ['*purify*']
 			}
 		})
